42b9592ad8357016c9b4d0695c9b3a9f54797f7b
Yara AlQuorashy <2473123A@student.gla.ac.uk>
2020-03-09 15:51:46 +0000

    Added base.html, added receipe-post.html, linked index and recipe post html pages to the base html, and proof read the recipe page.

--- breakingbread/urls.py
+++ breakingbread/urls.py
@@ -5,6 +5,7 @@ app_name = 'breakingbread'
 urlpatterns = [
 path('', views.index, name='index'),
 path('register/', views.register, name='register'),
+path('recipe/', views.recipe, name='recipe'),
 path('login/', views.user_login, name='login'),
 path('logout/', views.user_logout, name='logout'),
 
