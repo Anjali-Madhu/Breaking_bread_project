36719e994e5647ac2225ea80a29e2011fb3e3599
Sahil Rajarai <sahil-007@live.com>
2020-03-10 15:03:04 +0000

    modified login page

--- breakingbread/forms.py
+++ breakingbread/forms.py
@@ -6,12 +6,12 @@ from django.contrib.auth.models import User
 user_types=[(0,'Regular'),(1,'Chef'),(2,'Restaurant')]
 
 class SignUpForm(UserCreationForm):
-    username=forms.CharField(max_length=128,required=True, widget=forms.TextInput(attrs={'placeholder': 'Username'}))
-    first_name=forms.CharField(max_length=30,required=True,widget=forms.TextInput(attrs={'placeholder': 'FirstName'}))
-    last_name=forms.CharField(max_length=30,required=True,widget=forms.TextInput(attrs={'placeholder': 'LastName'}))
-    email = forms.EmailField(max_length=20,required=True,widget=forms.TextInput(attrs={'placeholder': 'Email'}))
-    password1=forms.CharField(label = "Password",widget=forms.PasswordInput(attrs={'placeholder': 'Password'}),required=True)
-    password2=forms.CharField(label = "Confirm Password",widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}),required=True)
+    username=forms.CharField(max_length=128,required=True, widget=forms.TextInput())
+    first_name=forms.CharField(max_length=30,required=True,widget=forms.TextInput())
+    last_name=forms.CharField(max_length=30,required=True,widget=forms.TextInput())
+    email = forms.EmailField(max_length=20,required=True,widget=forms.TextInput())
+    password1=forms.CharField(label = "Password",widget=forms.PasswordInput(),required=True)
+    password2=forms.CharField(label = "Confirm Password",widget=forms.PasswordInput(),required=True)
     class Meta:
         model = User
         fields = ('username','first_name','last_name', 'email', 'password1','password2')
@@ -21,7 +21,7 @@ class UserProfileForm(forms.ModelForm):
     usertype=forms.CharField(label="User Type  ",widget=forms.Select(choices=user_types),)
     usertype.widget.attrs.update({'class':'form-control'})
 
-    address = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'placeholder': 'Address'}))
+    address = forms.CharField(max_length=100,widget=forms.TextInput())
     
     class Meta:
         model=UserProfile
