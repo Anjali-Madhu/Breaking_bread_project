75d94cf7d73b249197823267f4175cd8842c6ea2
Razvan Constantin <constantinrzv@yahoo.com>
2020-03-17 22:12:08 +0000

    added rating

--- static/js/addons/rating.js
+++ static/js/addons/rating.js
@@ -1,34 +1,57 @@
-jQuery(document).ready(function($) {
-$('.rating_stars span.r').hover(function() {
-            // get hovered value
-            var rating = $(this).data('rating');
-            var value = $(this).data('value');
-            $(this).parent().attr('class', '').addClass('rating_stars').addClass('rating_'+rating);
-            highlight_star(value);
-        }, function() {
-            // get hidden field value
-            var rating = $("#rating").val();
-            var value = $("#rating_val").val();
-            $(this).parent().attr('class', '').addClass('rating_stars').addClass('rating_'+rating);
-            highlight_star(value);
-        }).click(function() {
-            // Set hidden field value
-            var value = $(this).data('value');
-            $("#rating_val").val(value);
-
-            var rating = $(this).data('rating');
-            $("#rating").val(rating);
-
-            highlight_star(value);
-        });
-
-        var highlight_star = function(rating) {
-            $('.rating_stars span.s').each(function() {
-                var low = $(this).data('low');
-                var high = $(this).data('high');
-                $(this).removeClass('active-high').removeClass('active-low');
-                if (rating >= high) $(this).addClass('active-high');
-                else if (rating == low) $(this).addClass('active-low');
-            });
-        }
+$(document).ready(function(){
+  
+  /* 1. Visualizing things on Hover - See next part for action on click */
+  $('#stars li').on('mouseover', function(){
+    var onStar = parseInt($(this).data('value'), 10); // The star currently mouse on
+   
+    // Now highlight all the stars that's not after the current hovered star
+    $(this).parent().children('li.star').each(function(e){
+      if (e < onStar) {
+        $(this).addClass('hover');
+      }
+      else {
+        $(this).removeClass('hover');
+      }
+    });
+    
+  }).on('mouseout', function(){
+    $(this).parent().children('li.star').each(function(e){
+      $(this).removeClass('hover');
+    });
+  });
+  
+  
+  /* 2. Action to perform on click */
+  $('#stars li').on('click', function(){
+    var onStar = parseInt($(this).data('value'), 10); // The star currently selected
+    var stars = $(this).parent().children('li.star');
+    
+    for (i = 0; i < stars.length; i++) {
+      $(stars[i]).removeClass('selected');
+    }
+    
+    for (i = 0; i < onStar; i++) {
+      $(stars[i]).addClass('selected');
+    }
+    
+    // JUST RESPONSE (Not needed)
+    var ratingValue = parseInt($('#stars li.selected').last().data('value'), 10);
+    var msg = "";
+    if (ratingValue > 1) {
+        msg = "Thanks! You rated this " + ratingValue + " stars.";
+    }
+    else {
+        msg = "We will improve ourselves. You rated this " + ratingValue + " stars.";
+    }
+    responseMessage(msg);
+    
+  });
+  
+  
 });
+
+
+function responseMessage(msg) {
+  $('.success-box').fadeIn(200);  
+  $('.success-box div.text-message').html("<span>" + msg + "</span>");
+}
