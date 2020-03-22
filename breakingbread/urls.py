9dabcb91c399683b2d18b11ea2f543dfe4c4d455
Anjali-Madhu-Menon <2378338M@student.gla.ac.uk>
2020-03-22 19:24:59 +0000

    modified models recpie and report

--- breakingbread/urls.py
+++ breakingbread/urls.py
@@ -19,4 +19,5 @@ path('browse/cuisine/<str:cuisine>/',views.search,name='browse_cuisine'),
 path('my-details/',views.user_details,name="details"),
 path('uploadrecipe/', views.upload_recipe, name='upload_recipe'),
 path('about/', views.about, name='about'),
+path('reviews/<int:recipe_id>/', views.review, name='reviews'),
 ]
--- populate_breakingbread.py
+++ populate_breakingbread.py
@@ -192,13 +192,13 @@ def populate():
     for img in Images:
         add_image(img['image_id'], img['picture'], img['recipe_id'])
 
-    Reviews = [{'review_id': 1, 'recipe_id': all_recipes[0], 'username': all_usersProfile[1],
+    Reviews = [{'review_id': 1, 'recipe_id': all_recipes[0], 'username': selected_users[1],
               'rating': 3, 'description': 'very tasty',   'date': '12/03/2020'},
-               {'review_id': 2, 'recipe_id': all_recipes[1], 'username': all_usersProfile[1],
+               {'review_id': 2, 'recipe_id': all_recipes[1], 'username': selected_users[1],
               'rating': 5, 'description': 'a very tasty recipe ',   'date': '12/03/2020'},
-              {'review_id': 3, 'recipe_id': all_recipes[2], 'username': all_usersProfile[0],
+              {'review_id': 3, 'recipe_id': all_recipes[2], 'username': selected_users[0],
               'rating': 5, 'description': 'very tasty',   'date': '12/03/2020'},
-               {'review_id': 4, 'recipe_id': all_recipes[3], 'username': all_usersProfile[1],
+               {'review_id': 4, 'recipe_id': all_recipes[3], 'username': selected_users[1],
               'rating': 2, 'description': 'interesting taste',   'date': '12/03/2020'},
                ]
     
